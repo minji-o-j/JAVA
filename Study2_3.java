7c56cf3ea61742b30fea63a6fc212958fcc29012
Minji <jminji98@gmail.com>
2019-08-04 11:42:30 +0900

    Create Study2_3.java

---
+++ Study2_3.java
@@ -0,0 +1,12 @@
+class Study2_3{
+  static int [] dan={1,2,3,4};
+  static int[] test={0,4,8,16,32};
+  public static void main(String[] args){
+    for(int i:dan){
+      System.out.println("dan: "+i);
+    }
+    for(int j:test){
+      System.out.println("test"+j);
+    }
+  }
+}
