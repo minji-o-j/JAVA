436b8660da06f9a5573d3776832c3eba42434a66
Minji <jminji98@gmail.com>
2020-02-17 00:01:23 +0900

    ShapeMain

---
+++ ShapeMain.java
@@ -0,0 +1,9 @@
+public class ShapeMain{
+
+  public static void main(String[] args){
+    Rectangle r=new Rectangle(3.,4.);
+    System.out.println("AREA: "+r.calcArea());
+    System.out.println("PERIMETER: "+r.calcPerimeter());
+
+  }
+}
