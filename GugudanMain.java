56183f883f11d78bae732b208b1f1d669a085c59
Minji <jminji98@gmail.com>
2020-03-19 14:57:55 +0900

    구구단 Main

---
+++ GugudanMain.java
@@ -0,0 +1,18 @@
+class Gugudan{
+  int[] dan={1,2,3,4,5,6,7,8,9};
+  public void printGugudan(){
+    for (int i=0;i<dan.length;i++){
+      System.out.printf("\n%d´Ü: ",dan[i]);
+      for(int j=0;j<dan.length;j++){
+        System.out.printf("%2dx%2d = %2d  ",dan[i],dan[j],dan[i]*dan[j]);
+      }
+    }
+  }
+}
+
+public class GugudanMain{
+  public static void main(String[] args){
+    Gugudan gd=new Gugudan();
+    gd.printGugudan();
+  }
+}
