23e45aed79d867cfb06d6927ac2a16ac44509852
Minji <jminji98@gmail.com>
2020-03-29 15:58:23 +0900

    Create TrMain.java

---
+++ TrMain.java
@@ -0,0 +1,6 @@
+public class TrMain{
+ public static void main(String[] args){
+   Triangle t= new Triangle(3,4);//same package(directory) --> import X!!
+   System.out.println(t.computeArea());
+ }
+}
