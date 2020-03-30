6f92351993deb500447b00903735b2f15f018075
Minji <jminji98@gmail.com>
2020-03-31 02:06:05 +0900

    Create Study2_1.java

---
+++ Study2_1.java
@@ -0,0 +1,33 @@
+class Study2_1{
+    // static int a=1;
+    // static float b=2;
+    // static float c=2.0f;
+    //  static double d=3;
+    // static double e=3.0;
+    // static long f=4325839280L;
+    static String g="world";
+    static char h='h';
+    public static void main(String []args){
+      // if(a==1){
+      //   System.out.println("one");
+      // }
+      // if(b==2.0f){
+      //   System.out.println("two");
+      // }
+      // else if(c==2){
+      //   System.out.println("three");
+      // }
+      // if(d==3.0f){
+      //   System.out.println("four");
+      // }
+      // if(e==(int)3){
+      //   System.out.println("five");
+      // }
+      if(g=="world"){
+        System.out.println("g is world");
+      }
+      else{
+        System.out.println("g is not world");
+      }
+    }
+}
