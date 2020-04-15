1e47a6b3e149222c2ccce5395b2bbe93c3f04d54
Minji <jminji98@gmail.com>
2020-04-16 01:38:57 +0900

    Create O4_X5_NumbersMain.java

---
+++ O4_X5_NumbersMain.java
@@ -0,0 +1,7 @@
+public class O4_X5_NumbersMain{
+  public static void main(String[] args){
+    O4_X5_Numbers n=new O4_X5_Numbers();
+    n.setNumbers();
+    System.out.print("\nÇÕ°è: "+n.sumList());
+  }
+}
